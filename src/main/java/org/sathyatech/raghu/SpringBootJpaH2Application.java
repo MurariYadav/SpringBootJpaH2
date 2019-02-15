717a3aa18c1210f9c2c4816ad7a065a8c15d8603
javabyraghu <javabyraghu@gmail.com>
2019-02-15 23:45:54 +0530

    First Commit

---
+++ src/main/java/org/sathyatech/raghu/SpringBootJpaH2Application.java
@@ -0,0 +1,14 @@
+package org.sathyatech.raghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaH2Application.class, args);
+	}
+
+}
+
